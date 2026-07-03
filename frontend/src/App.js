14d1a85721463436db920bf5189430ef773bacf2
Srushti750 <kulkarnisrk2002@gmail.com>
2026-07-03 18:07:38 +0530

    Added the login user feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
@@ -9,6 +10,12 @@ function App() {
         path = "/register"
         element = {<RegisterPage/>}
       />
+
+      <Route
+        path = "/login"
+        element = {<LoginPage/>}
+      />
+      
     </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import { loginUser } from "../services/authServices";
+
+function LoginPage() {
+
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    });
+
+    const [message, setMessage] = useState("");
+
+    const handleChange = (event) => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value
+        });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await loginUser(formData);
+
+            if (response.success) {
+                localStorage.setItem(
+                    "access_token",
+                    response.access_token
+                );
+
+                setMessage("Login successful");
+            } else {
+                setMessage(response.message);
+            }
+
+        } catch (error) {
+            setMessage("Login failed");
+        }
+    };
+
+    return (
+        <div>
+            <h1>Login</h1>
+
+            <form onSubmit={handleSubmit}>
+
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    value={formData.email}
+                    onChange={handleChange}
+                />
+
+                <br /><br />
+
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    value={formData.password}
+                    onChange={handleChange}
+                />
+
+                <br /><br />
+
+                <button type="submit">
+                    Login
+                </button>
+
+            </form>
+
+            <p>{message}</p>
+
+        </div>
+    );
+}
+
+export default LoginPage;
